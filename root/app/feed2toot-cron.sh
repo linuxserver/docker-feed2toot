cd411dd7d808c46461f4b2b7368d3ef9134f06e1
Eric Nemchik <eric@nemchik.com>
2023-07-30 01:13:37 -0500

    standard cron
    Signed-off-by: Eric Nemchik <eric@nemchik.com>

--- root/app/feed2toot-cron.sh
+++ root/app/feed2toot-cron.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
-s6-setuidgid abc python3 /lsiopy/bin/feed2toot -l "${FEED_LIMIT:-5}" -c /config/feed2toot.ini
+python3 /lsiopy/bin/feed2toot -l "${FEED_LIMIT:-5}" -c /config/feed2toot.ini
